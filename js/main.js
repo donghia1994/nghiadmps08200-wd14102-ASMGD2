6330de2d5b4a67be66018c0310032e6b2084c1be
donghia1994 <55423896+donghia1994@users.noreply.github.com>
2019-12-18 23:46:55 +0700

    bosungASM

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-var app = angular.module('listMonhoc', []);
+var app = angular.module('myApp', ["ngRoute"]);
+app.config(function($routeProvider){
+    $routeProvider.when("/1",{
+        templateUrl : "template/main.html"
+    }).when("/quiz",{
+        templateUrl : "template/quiz.html"
+    }).otherwise({
+        redirectTo : "main.html"
+    });
+    // .otherwise({
+    //     templateUrl : "index.html"
+    // });
+});
 app.controller('myCtrllist',function($scope,$http){
     $http.get('js/data.json').then(function(res){
         $scope.lists = res.data;
+    })
+    $http.get('data/quiz.json').then(function(res){
+        $scope.quizs = res.data;
     })
 // $scope.data = studys;
 // $http.get('js/data.json').success(function(sub){
