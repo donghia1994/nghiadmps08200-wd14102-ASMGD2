909d16a43cc575f26a1c289a7cee8e7c140809f7
donghia1994 <55423896+donghia1994@users.noreply.github.com>
2019-12-19 18:45:59 +0700

    update

--- js/main.js
+++ js/main.js
@@ -2,21 +2,35 @@ var app = angular.module('myApp', ["ngRoute"]);
 app.config(function($routeProvider){
     $routeProvider.when("/home",{
         templateUrl : "template/main.html"
-        
     }).when("/quiz",{
         templateUrl : "template/quiz.html"
     }).otherwise({
         redirectTo : "/home"
     });
-  
+    // .otherwise({
+    //     templateUrl : "index.html"
+    // });
 });
 app.controller('myCtrllist',function($scope,$http){
+    $scope.totalMark = 0;
     $http.get('js/data.json').then(function(res){
         $scope.lists = res.data;
     })
     $http.get('data/quiz.json').then(function(res){
         $scope.quizs = res.data;
     })
+
+    $scope.checkQuiz = function(idx, ansId, mark){
+        var answid = ansId;
+        var myans = $scope.model[idx];
+        alert($scope.model[idx] + ", " + answid);
+        $scope.totalMark += (answid==myans)?mark:0;
+
+        alert($scope.totalMark);
+    };
+    $scope.model = [];
+    $scope.model[0] = "";
+    
 // $scope.data = studys;
 // $http.get('js/data.json').success(function(sub){
 //     $scope.data = sub; 
